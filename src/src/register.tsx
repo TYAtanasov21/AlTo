cc544994f2b45cb7a0bb636b0c125ec09498c7ef
TYAtanasov21 <TYAtanasov21@codingburgas.bg>
2024-02-08 11:51:34 +0200

    Make changes to register

--- src/src/register.tsx
+++ src/src/register.tsx
@@ -1,28 +1,28 @@
-import React, { useState} from 'react';
+import React, { useState, useRef } from 'react';
 
 function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [step, setStep] = useState(1);
+  const [user, setUser] = useState({ name: '', pass: '' });
 
-  const handleEmailSubmit = (event) => {
-    // const textInput = useRef!<HTMLInputElement>(null);
-    event.preventDefault();
+  const emailInput = useRef();
 
-    setStep(2);
-  };
-
-  const handlePasswordSubmit = (event) => {
-    event.preventDefault();
-    // Handle registration logic with email and password
-    console.log('Registration successful:', { email, password });
+  const handleClick = () => {
+    if (step === 1 && emailInput.current) {
+      setStep(2);
+    } else if (step === 2) {
+      console.log('Registration successful:', { email, password });
+      setUser({ name: email, pass: password });
+      console.log(user);
+    }
   };
 
   return (
     <div className="d-flex flex-column align-items-center justify-content-center vh-100">
       <h2 className="text-light mb-3">Register</h2>
       {step === 1 ? (
-        <form className="my-form p-4 rounded text-light" onSubmit={handleEmailSubmit}>
+        <form className="my-form p-4 rounded text-light">
           <div className="mb-3">
             <label htmlFor="exampleInputEmail" className="form-label visually-hidden">
               Email address
@@ -30,6 +30,7 @@ function Register() {
             <input
               type="email"
               className="form-control"
+              ref={emailInput}
               id="exampleInputEmail"
               placeholder="your_email@gmail.com"
               value={email}
@@ -37,13 +38,13 @@ function Register() {
             />
           </div>
           <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-light btn-lg">
+            <button type="button" className="btn btn-light btn-lg" onClick={handleClick}>
               Continue
             </button>
           </div>
         </form>
       ) : (
-        <form className="my-form p-4 rounded text-light" onSubmit={handlePasswordSubmit}>
+        <form className="my-form p-4 rounded text-light">
           <div className="mb-3">
             <label htmlFor="exampleInputPassword" className="form-label visually-hidden">
               Password
@@ -58,7 +59,7 @@ function Register() {
             />
           </div>
           <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-light btn-lg">
+            <button type="button" className="btn btn-light btn-lg" onClick={handleClick}>
               Register
             </button>
           </div>
