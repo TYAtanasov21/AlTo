3c5ac06c4e9d864fca6bc596f3be0e621f52442f
AZKazakov21 <AZKazakov21@codingburgas.bg>
2024-02-13 09:16:37 +0200

    Add dragging functionality to progress bar

--- src/UI Files/mainApp.tsx
+++ src/UI Files/mainApp.tsx
@@ -7,7 +7,7 @@ import "../css/app.css"
 
 export default function AppLayout({children}: {children:React.ReactNode}) {
     
-const audioFileUrl = "https://altosongstorage.blob.core.windows.net/songs/MBT%20-%20SUJALQVAM%20(OFFICIAL%20AUDIO).mp3?sp=r&st=2024-02-12T19:03:27Z&se=2024-02-13T03:03:27Z&spr=https&sv=2022-11-02&sr=b&sig=Fj%2Bsb2STuPUOljsYEzLhFXpujwYBexdVbyBs%2FpOtjK0%3D"; // Replace with your Azure Blob Storage URL
+const audioFileUrl = "https://altosongstorage.blob.core.windows.net/songs/MBT%20-%20SUJALQVAM%20(OFFICIAL%20AUDIO).mp3?sp=r&st=2024-02-13T06:19:52Z&se=2024-02-13T14:19:52Z&spr=https&sv=2022-11-02&sr=b&sig=2%2FDjIAR1CvOCHC3%2BxC1WnUCdyMPGN18XeehHtZzmRvc%3D"; // Replace with your Azure Blob Storage URL
   
   const sound = new Audio();
   sound.src = audioFileUrl;
