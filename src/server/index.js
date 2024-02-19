ec6484f571217e8ed97ab3dffcbf27d6264e9613
TYAtanasov21 <TYAtanasov21@codingburgas.bg>
2024-02-19 10:37:58 +0200

    change port

--- src/server/index.js
+++ src/server/index.js
@@ -249,7 +249,7 @@ app.post("/api/getLikedSongs", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
