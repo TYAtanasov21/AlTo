347175964c2cccd7e3be0907a6335265014e8bbb
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2024-03-09 00:15:36 +0200

    Edit register

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,10 +8,16 @@ const {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
-  darkMode: ["class"],
+  darkMode: "class",
   theme: {
+    // rest of the code
+    extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+    },
   },
-  plugins: [require("@tailwindcss/aspect-ratio"), addVariablesForColors],
+  plugins: [addVariablesForColors],
 };
 
 function addVariablesForColors({ addBase, theme }) {
