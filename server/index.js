344d3ebe0aa94074ee1a47763c8d35e9ca5a4ac8
TYAtanasov21 <TYAtanasov21@codingburgas.bg>
2024-03-11 13:59:37 +0200

    Edit server

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ import playlistRoutes from './routes/playlistRoutes.js';
 const { Pool } = pkg;
 const app = express();
 const PORT = process.env.PORT || 5000;
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
